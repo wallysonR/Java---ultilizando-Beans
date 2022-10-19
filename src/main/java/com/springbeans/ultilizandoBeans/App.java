3a33d37014c7432ae9711914378775980b407451
Wallyson Roberto Leite dos Santos <wallysons@ciandt.com>
2022-10-19 11:14:47 -0300

    Teste commit

--- src/main/java/com/springbeans/ultilizandoBeans/App.java
+++ src/main/java/com/springbeans/ultilizandoBeans/App.java
@@ -1,26 +1,19 @@
 package com.springbeans.ultilizandoBeans;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
         Livro livro = factory.getBean(Livro.class);
         Autor autor = factory.getBean(Autor.class);
-        Autor auto = factory.getBean(Autor.class);
-        Livro livr = factory.getBean(Livro.class);
         livro.setNome("Vigiar e Punir");
         livro.setCodigo("bn912");
         autor.setNome("Michel Foucault");
-        livr.setNome("Madame Bovary");
-        livr.setCodigo("bn913");
-        auto.setNome("Gustave Flaubert");
         livro.exibir();
-        livr.exibir();
-        try {
-            factory.close();
-        } finally {
-            System.out.println("ERROR");
-        }
+        ((AbstractApplicationContext) factory).close();
+        System.out.println("Teste");
     }
 }
